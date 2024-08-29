99946ddc7811ead6de01555bc2e9007ba03db7b7
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-08-29 17:20:57 +0600

    Update

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,8 +9,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
-  bool icecream=false,pizza=false,salad=false,burger=false;
+  bool icecream = false, pizza = false, salad = false, burger = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,111 +22,180 @@ class _HomeState extends State<Home> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Hello Sakib,",
-                    style:AppWidget.boldTextFieldStyle()
-                ),
+                Text("Hello Ishrak,", style: AppWidget.boldTextFieldStyle()),
                 Container(
                   padding: EdgeInsets.all(3),
-                  decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(8)),
-                  child: Icon(Icons.shopping_cart, color: Colors.white,),
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Icon(
+                    Icons.shopping_cart,
+                    color: Colors.white,
+                  ),
                 )
               ],
             ),
-            SizedBox(height: 20.0,),
-            Text("Delicious Food",
-                style:AppWidget.HeadLineTextFieldStyle()
+            SizedBox(
+              height: 20.0,
             ),
+            Text("Delicious Food", style: AppWidget.HeadLineTextFieldStyle()),
             Text("Discover & Get Great Food",
-                style:AppWidget.LightTextFieldStyle()
+                style: AppWidget.LightTextFieldStyle()),
+            SizedBox(
+              height: 20.0,
             ),
+            
+            showItem(),
             SizedBox(height: 20.0,),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-              GestureDetector(
-                onTap: (){
-                  pizza=true;
-                  burger=false;
-                  salad=false;
-                  icecream=false;
-                  setState(() {
-
-                  });
-                },
-                child: Material(
+                Material(
                   elevation: 5.0,
-                  borderRadius: BorderRadius.circular(10),
                   child: Container(
-                    decoration: BoxDecoration(color: pizza?Colors.orange :Colors.white, borderRadius: BorderRadius.circular(8)),
-                    padding: EdgeInsets.all(6) ,
-                    child: Image.asset("images/pizza.png",height: 60,width: 60,fit: BoxFit.cover,color: pizza?Colors.white:Colors.orange),
-                  ),
-                ),
-              ),
-                GestureDetector(
-                  onTap: (){
-                    pizza=false;
-                    burger=true;
-                    salad=false;
-                    icecream=false;
-                    setState(() {
-
-                    });
-                  },
-                  child: Material(
-                    elevation: 5.0,
-                    borderRadius: BorderRadius.circular(10),
-                    child: Container(
-                      decoration: BoxDecoration(color: burger?Colors.orange :Colors.white, borderRadius: BorderRadius.circular(8)),
-                      padding: EdgeInsets.all(6) ,
-                      child: Image.asset("images/burger.png",height: 60,width: 60,fit: BoxFit.cover,color: burger?Colors.white:Colors.orange),
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 25.0,
+                        ),
+                        Image.asset(
+                          "images/pizzareal.png",
+                          height: 150,
+                          width: 150,
+                          fit: BoxFit.cover,
+                        ),
+                        Text(
+                          "Mashroom Pizza",
+                          style: AppWidget.SemiBoldTextFieldStyle(),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Text(
+                          "So Healthy & Delicious",
+                          style: AppWidget.LightTextFieldStyle(),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Text(
+                          "\$50",
+                          style: AppWidget.SemiBoldTextFieldStyle(),
+                        )
+                      ],
                     ),
                   ),
-                ),
-                GestureDetector(
-                  onTap: (){
-                    pizza=false;
-                    burger=false;
-                    salad=true;
-                    icecream=false;
-                    setState(() {
-
-                    });
-                  },
-                  child: Material(
-                    elevation: 5.0,
-                    borderRadius: BorderRadius.circular(10),
-                    child: Container(
-                      decoration: BoxDecoration(color: salad?Colors.orange :Colors.white, borderRadius: BorderRadius.circular(8)),
-                      padding: EdgeInsets.all(6) ,
-                      child: Image.asset("images/salad.png",height: 60,width: 60,fit: BoxFit.cover,color: salad?Colors.white:Colors.orange),
-                    ),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: (){
-                    pizza=false;
-                    burger=false;
-                    salad=false;
-                    icecream=true;
-                    setState(() {
-
-                    });
-                  },
-                  child: Material(
-                    elevation: 5.0,
-                    borderRadius: BorderRadius.circular(10),
-                    child: Container(
-                      decoration: BoxDecoration(color: icecream?Colors.orange :Colors.white, borderRadius: BorderRadius.circular(8)),
-                      padding: EdgeInsets.all(6) ,
-                      child: Image.asset("images/icecream.png",height: 60,width: 60,fit: BoxFit.cover,color: icecream?Colors.white:Colors.orange),
-                    ),
-                  ),
-                ),
-            ],)
+                )
+              ],
+            )
           ],
         ),
       ),
+    );
+  }
+
+  Widget showItem() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        GestureDetector(
+          onTap: () {
+            pizza = true;
+            burger = false;
+            salad = false;
+            icecream = false;
+            setState(() {});
+          },
+          child: Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: pizza ? Colors.orange : Colors.white,
+                  borderRadius: BorderRadius.circular(8)),
+              padding: EdgeInsets.all(6),
+              child: Image.asset("images/pizza.png",
+                  height: 60,
+                  width: 60,
+                  fit: BoxFit.cover,
+                  color: pizza ? Colors.white : Colors.black),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            pizza = false;
+            burger = true;
+            salad = false;
+            icecream = false;
+            setState(() {});
+          },
+          child: Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: burger ? Colors.red : Colors.white,
+                  borderRadius: BorderRadius.circular(8)),
+              padding: EdgeInsets.all(6),
+              child: Image.asset("images/burger.png",
+                  height: 60,
+                  width: 60,
+                  fit: BoxFit.cover,
+                  color: burger ? Colors.white : Colors.black),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            pizza = false;
+            burger = false;
+            salad = true;
+            icecream = false;
+            setState(() {});
+          },
+          child: Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: salad ? Colors.lightGreen : Colors.white,
+                  borderRadius: BorderRadius.circular(8)),
+              padding: EdgeInsets.all(6),
+              child: Image.asset("images/salad.png",
+                  height: 60,
+                  width: 60,
+                  fit: BoxFit.cover,
+                  color: salad ? Colors.white : Colors.black),
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            pizza = false;
+            burger = false;
+            salad = false;
+            icecream = true;
+            setState(() {});
+          },
+          child: Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: icecream ? Colors.pinkAccent : Colors.white,
+                  borderRadius: BorderRadius.circular(8)),
+              padding: EdgeInsets.all(6),
+              child: Image.asset("images/icecream.png",
+                  height: 60,
+                  width: 60,
+                  fit: BoxFit.cover,
+                  color: icecream ? Colors.white : Colors.black),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/widget/widget_support.dart
+++ lib/widget/widget_support.dart
@@ -24,4 +24,11 @@ class AppWidget{
         fontWeight: FontWeight.w500,
         fontFamily: "Poppins");
   }
+  static TextStyle SemiBoldTextFieldStyle(){
+    return TextStyle(
+        color: Colors.black,
+        fontSize: 18.0,
+        fontWeight: FontWeight.w500,
+        fontFamily: "Poppins");
+  }
 }
